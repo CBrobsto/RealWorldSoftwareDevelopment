02f0bef7f4ca8bce64b075a6d82890ad997fb8e3
Cody Brobston <cbrobston22@gmail.com>
2020-12-09 16:51:15 -0800

    Report importer implemented.

--- src/main/java/chapter4/DocumentManagementSystem.java
+++ src/main/java/chapter4/DocumentManagementSystem.java
@@ -19,6 +19,7 @@ public class DocumentManagementSystem {
     public DocumentManagementSystem() {
         extensionToImporter.put("letter", new LetterImporter());
         extensionToImporter.put("invoice", new InvoiceImporter());
+        extensionToImporter.put("report", new ReportImporter());
     }
 
     public void importFile(final String path) throws IOException {
