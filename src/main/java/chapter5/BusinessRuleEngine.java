adb7a36edc184c0f00d6dba76c1ef6778042e5c5
Cody Brobston <cbrobston22@gmail.com>
2020-12-10 12:25:06 -0800

    Declared basic API

---
+++ src/main/java/chapter5/BusinessRuleEngine.java
@@ -0,0 +1,16 @@
+package main.java.chapter5;
+
+public class BusinessRuleEngine {
+
+    public void addAction(final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+}
