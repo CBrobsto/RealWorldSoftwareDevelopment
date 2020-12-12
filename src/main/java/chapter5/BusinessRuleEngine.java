eeedf241bfc4657011a235ca5667c7ddcd875c04
Cody Brobston <cbrobston22@gmail.com>
2020-12-11 19:57:47 -0800

    Implemented BusinessRuleEngine methods to pass the unit tests.

--- src/main/java/chapter5/BusinessRuleEngine.java
+++ src/main/java/chapter5/BusinessRuleEngine.java
@@ -1,13 +1,22 @@
 package main.java.chapter5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessRuleEngine {
 
+    private final List<Action> actions;
+
+    public BusinessRuleEngine() {
+        this.actions = new ArrayList<>();
+    }
+
     public void addAction(final Action action) {
-        throw new UnsupportedOperationException();
+        this.actions.add(action);
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return this.actions.size();
     }
 
     public void run() {
