629e144810f12d3f7bb2a3342a677d0c21739e76
Cody Brobston <cbrobston22@gmail.com>
2020-12-14 13:37:10 -0800

    Added mocking test for execute method. Issue with Mockito jar.

--- src/test/java/chapter5/BusinessRuleEngineTest.java
+++ src/test/java/chapter5/BusinessRuleEngineTest.java
@@ -1,8 +1,11 @@
 package test.java.chapter5;
 
+import main.java.chapter5.Action;
 import main.java.chapter5.BusinessRuleEngine;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 
 public class BusinessRuleEngineTest {
 
@@ -21,5 +24,16 @@ public class BusinessRuleEngineTest {
         businessRuleEngine.addAction(() -> {});
 
         Assertions.assertEquals(2, businessRuleEngine.count());
+    }
+
+    @Test
+    void shouldExecuteOneAction() {
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+        final Action mockAction = Mockito.mock(Action.class);
+
+        businessRuleEngine.addAction(mockAction);
+        businessRuleEngine.run();
+
+        Mockito.verify(mockAction).execute();
     }
 }
